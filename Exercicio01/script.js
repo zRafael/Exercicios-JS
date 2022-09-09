ef1107df1ca2f689493f9442e0e587da02721494
zRafael <rafael.silva@bandtec.com.br>
2022-09-09 20:32:56 -0300

    Modificando exercicio 01

--- Exercicio01/script.js
+++ Exercicio01/script.js
@@ -2,7 +2,7 @@ function carregar() {
     var msg = window.document.getElementById('msg')
     var img = window.document.getElementById('img')
     var data = new Date()
-    var hora = 22
+    var hora = data.getHours()
 
     msg.innerHTML = `Agora são ${hora} horas.`
 
